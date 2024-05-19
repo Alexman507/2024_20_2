f0cc71a4d954b66517ea3f3f673f86991160dc68
NeroPC <Nerocross507@gmail.com>
2024-05-20 00:51:53 +0300

    fill admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from main.models import Category, Product, Contact
+
+"""
+Для категорий выводит **id** и **наименование** в список отображения, 
+а для продуктов выводит в список **id**, **название**, **цену** и **категорию**.
+
+Интерфейс вывода продуктов:
+- результат отображения фильтровать по категории, 
+- поиск по названию и полю описания.
+"""
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Contact)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "phone", "message", "time")
